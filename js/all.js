6a9ad8ebe6f00cfe78d3f41afbea2de0109d282a
Vetrivel K Subramaniam <vetrivictorious@gmail.com>
2016-10-23 14:10:02 +0530

    Changes

--- js/all.js
+++ js/all.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
-$(".navIcon").click(
+	function addAnim(className)
+	{
+		var winHeight= $(window).height();
+		var eleHeight= $(className).height();
+		var transY = -( winHeight -  eleHeight);
+		var cssProp = "translate(0px,"+transY+"px)";
+		var kits = ["transform","-webkit-transform","-moz-transform","-o-transform"];
+		for(var kitType in kits)
+		{
+			$(className).css(kits[kitType],cssProp);
+		}
+	}
+	$(window).resize(function(){
+		addAnim(".moveUp","0");
+	});
+	$(".navIcon").click(
 		function()
 		{
-			$(".home,.navbar").addClass("moveUp");
+			$(".navBar").addClass("moveUp");
+			addAnim(".moveUp","0");
+			$(".home").addClass("hide");
+			$(".home").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){$(".home").css("display","none"); });
 		});
 });
