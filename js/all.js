9e288c69147a5296333a1a8c0c974a8cc12c087a
Vetrivel K Subramaniam <vetrivictorious@gmail.com>
2016-11-26 11:34:25 +0530

    init

--- js/all.js
+++ js/all.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
-	function addAnim(className)
-	{
-		var winHeight= $(window).height();
-		var eleHeight= $(className).height();
-		var transY = -( winHeight -  eleHeight);
-		var cssProp = "translate(0px,"+transY+"px)";
-		var kits = ["transform","-webkit-transform","-moz-transform","-o-transform"];
-		for(var kitType in kits)
-		{
-			$(className).css(kits[kitType],cssProp);
-		}
-	}
-	$(window).resize(function(){
-		addAnim(".moveUp","0");
-	});
-	$(".navIcon").click(
-		function()
-		{
-			$(".navBar").addClass("moveUp");
-			addAnim(".moveUp","0");
-			$(".home").addClass("hide");
-			$(".home").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",
-					function(){
-						$(".home").css("display","none");
-						});
-		});
+    function addAnim(className)
+    {
+        var winHeight= $(window).height();
+        var eleHeight= $(className).height();
+        var transY = -( winHeight -  eleHeight);
+        var cssProp = "translate(0px,"+transY+"px)";
+        var kits = ["transform","-webkit-transform","-moz-transform","-o-transform"];
+        for(var kitType in kits)
+        {
+            $(className).css(kits[kitType],cssProp);
+        }
+    }
+    $(window).resize(function(){
+        addAnim(".moveUp","0");
+    });
+    $(".navIcon").click(
+        function()
+        {
+            $(".navBar").addClass("moveUp");
+            addAnim(".moveUp","0");
+            $(".home").addClass("hide");
+            $(".home").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",
+                    function(){
+                        $(".home").css("display","none");
+                        });
+        });
 });
