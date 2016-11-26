0224f199ea6688993f582e10071c921711cd42a4
Vetrivel K Subramaniam <vetrivictorious@gmail.com>
2016-11-26 13:26:20 +0530

    img

--- js/all.js
+++ js/all.js
@@ -17,12 +17,22 @@ $(document).ready(function() {
     $(".navIcon").click(
         function()
         {
-            $(".navBar").addClass("moveUp");
-            addAnim(".moveUp","0");
-            $(".home").addClass("hide");
-            $(".home").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",
+//            $(".navBar").addClass("moveUp");
+  //          addAnim(".moveUp","0");
+            $(".homePage").addClass("hide");
+           /* $(".home").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",
                     function(){
                         $(".home").css("display","none");
-                        });
-        });
+                        });*/
+    });
+    var design66 = [];
+    design66 = [
+                {dispName:"day0", img:"0.jpg"},
+                {dispName:"day1", img:"1.jpg"},
+                {dispName:"day2", img:"2.jpg"}
+                ];
+    for(var item = 0; item < design66.length; item++ )
+        {
+            $('.portfolioPage').append('<div class="item '+design66[item].dispName+'"><img src='+"img//"+design66[item].img+' width="200px" height="200px"></img></div>');
+        }
 });
